4b3d9caac6dd1d30fa9578cc41dc332d744c7775
Cedric ZH <cedriczhchan@gmail.com>
2016-12-18 17:47:27 +0800

    Remove placeholder files

--- product.rb
+++
@@ -1,8 +0,0 @@
-class Product
-  attr_accessor :code, :name, :price
-  def initialize(**args)
-    @code = args[:code]
-    @name = args[:name]
-    @price = args[:price]
-  end
-end
--- spec/foo_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rspec'
-require './checkout'
-RSpec.describe Checkout do
-  it { expect(1+1).to eq(2) }
-end
